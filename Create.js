d56e6b574c1fd9229ceb08e22fa953c0d2e0118c
Muhammad Asjad <113926414+Cario@users.noreply.github.com>
2022-12-13 12:46:25 -0500

    Update Create.js
    Updated website to include error messages whenever something goes wrong when adding a game

--- Create.js
+++ Create.js
@@ -6,11 +6,17 @@ function sendGame(){
     let platform = document.getElementById("platform").value;
     platform = platform.trim();
     console.log(platform);
-    if(gameName == null || gameName == ""){
+   if((gameName == null || gameName == "") && (platform == null || platform == "")){
+        console.log("Error: Empty GameName and Platform Field")
+        document.getElementById("hey").innerHTML = "Error: Empty GameName and Platform Field";
+    }
+    else if(gameName == null || gameName == ""){
         console.log("Error: Empty GameName Field")
+        document.getElementById("hey").innerHTML = "Error: Empty GameName Field";
     }
     else if(platform == null || platform == ""){
         console.log("Error: Empty PLatform Field")
+        document.getElementById("hey").innerHTML = "Error: Empty Platform Field";
     }
     else{
         let retData = `{"name" : "${gameName}", "platform" : "${platform}"}`
